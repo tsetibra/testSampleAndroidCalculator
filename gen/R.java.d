9114262a72edaef3922c68a58cb0c1a3ac841367
Ibraheem Zaman <ibraheem.zaman@arbisoft.com>
2013-05-07 17:07:50 +0500

    dummy commit

--- gen/R.java.d
+++
@@ -1,7 +0,0 @@
-/home/naveed/Workspace/testAndroidCalculator/gen/com/testcalculator/R.java \
- : /home/naveed/Workspace/testAndroidCalculator/res/layout/main.xml \
-/home/naveed/Workspace/testAndroidCalculator/res/drawable-mdpi/ic_launcher.png \
-/home/naveed/Workspace/testAndroidCalculator/res/values/strings.xml \
-/home/naveed/Workspace/testAndroidCalculator/res/drawable-hdpi/ic_launcher.png \
-/home/naveed/Workspace/testAndroidCalculator/res/drawable-ldpi/ic_launcher.png \
-/home/naveed/Workspace/testAndroidCalculator/bin/AndroidManifest.xml \
